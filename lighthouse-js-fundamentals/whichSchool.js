1cce248bfe9dd0a8a7b3f7510017c3c1b0020919
Julian Bustos <julian.m.bustos@gmail.com>
2020-04-29 17:17:21 +0000

    Add whichSchool to go to based on age

---
+++ lighthouse-js-fundamentals/whichSchool.js
@@ -0,0 +1,12 @@
+let age = 7
+let whichSchool 
+
+if (age < 13) {
+  whichSchool = "Elementary School";
+} else if (age <= 18) {
+  whichSchool = "Secondary School";
+} else if (age > 18) {
+  whichSchool = "Lighthouse Labs";
+}
+
+console.log(whichSchool)
